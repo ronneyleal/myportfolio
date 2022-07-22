ffa0a994f7cf557491ae427230e3c2a135cb1697
Ronney Leal <ronneyharper@gmail.com>
2022-07-22 08:31:00 -0300

    swiper

---
+++ js/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 60,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
