d0f28bbdf0a5cc0f32b478fc2042dfa64bb2ac51
Ronney Leal <ronneyharper@gmail.com>
2022-07-27 09:13:08 -0300

    contact

--- js/script.js
+++ js/script.js
@@ -1,18 +1,92 @@
-// PARALLAX //
+// ALTERAR LOGO QUANDO DER HOVER
 
-document.addEventListener("mousemove", parallax);
-function parallax(e) {
-  this.querySelectorAll('.layer').forEach(layer => {
-    const speed = layer.getAttribute('data-speed')
+function changelogo() {
+  let ronneydev = document.getElementById('ronney')
 
-    const x = (window.innerWidth - e.pageX*speed)/100
-    const y = (window.innerHeight - e.pageY*speed)/100
-
-    layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-  });
+  if (
+    ronneydev.addEventListener('mouseover', function (e) {
+      ronneydev.src = 'images/ronneydev2.png'
+    })
+  );
+  else
+    ronneydev.addEventListener('mouseout', function (e) {
+      ronneydev.src = 'images/ronneydev.png'
+    })
 }
 
-// END PARALLAX //
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '8rem',
+  duration: 1000,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .aside, #home .aside-r,
+  #about .about-img, #about .about-text,
+  #courses .courses-title, #courses .content,
+  #certificates .cert-title, #certificates .box-cert,
+  #contact .box-form, #contact .box-media,
+  footer .box-footer, footer .footer-icons, footer .footer-links
+  `,
+  { interval: 100 }
+)
+
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 2
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
+
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
 
 /* mudar o header da página quando der scroll */
 const header = document.querySelector('#header')
@@ -31,19 +105,21 @@ function changeHeaderWhenScroll() {
 /* When Scroll */
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
-
+  backToTop()
+  activateMenuAtCurrentSection()
 })
 
-document.addEventListener('mouseenter', changelogo)
+// alterar cor de fundo conforme icones
 
-function changelogo() {
-  let ronneydev = document.getElementById('ronney')
-  
-  if (ronneydev.addEventListener('mouseover', function (e) {
-      ronneydev.src = "images/ronneydev2.png"
-  })); 
-  else (ronneydev.addEventListener('mouseout', function (e) {
-    ronneydev.src = "images/ronneydev.png"
-}));
-}
-
+let list = document.querySelectorAll('.box-icons li')
+let bg = document.querySelector('body')
+list.forEach(elements => {
+  elements.addEventListener('mouseenter', function (e) {
+    let color = e.target.getAttribute('data-color')
+    bg.style.background = color
+  })
+  elements.addEventListener('mouseleave', function (e) {
+    bg.style.background =
+      'linear-gradient(-220deg, var(--second-color), var(--light-color))'
+  })
+})
--- js/swiper.js
+++ js/swiper.js
@@ -1,16 +1,51 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper = new Swiper('.courses-swiper', {
   slidesPerView: 3,
-  spaceBetween: 60,
+  spaceBetween: 30,
   slidesPerGroup: 1,
+  initialSlide: 1,
   loop: true,
-  loopFillGroupWithBlank: true,
+  centeredSlides: true,
+  centeredSlidesBounds: true,
+  // 'slide' | 'fade' | 'cube' | 'coverflow' | 'flip' | 'creative' | 'cards'
+  effect: 'slide',
+  grabCursor: true,
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+    el: '.swiper-pagination',
+    clickable: true
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
   },
-});
 
+  breakpoints: {
+    350: {
+      slidesPerView: 1,
+      spaceBetween: 10
+    },
+
+    420: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    580: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+
+    650: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 40
+    }
+  }
+})
